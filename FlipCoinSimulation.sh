03d3e77d2fc0e6224fcbf08b4c60ea0d955608ab
Vikash Kanna <Enter Git Registered Email id>
2020-10-15 09:29:37 +0530

    Multiple Flips

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -2,10 +2,19 @@
 
 echo "Welcome to Flip Coin Simulation Problem"
 
-a=$(( RANDOM%2 ));
-if [ $a == 1 ];
-then
-         echo "Head is winner"
-else
-        echo "Tail is winner"
-fi
+head=0;
+tail=0;
+read -p "Enter number of flip required" n
+for (( i=0; i<n; i++ ))
+do
+
+	a=$(( RANDOM%2 ));
+	if [ $a == 1 ];
+	then
+         	head=$(($head+1))
+	else
+        	tail=$(($tail+1))
+	fi
+done
+echo "Number of head win" $head
+echo "Number of tail win" $tail

8f916906a9d6e6753a79ddbfb0fd1917b9445ceb
Vikash Kanna <Enter Git Registered Email id>
2020-10-15 09:25:47 +0530

    FlipCoin Heads or Tails

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,3 +1,11 @@
 #!/usr/local/bin/bash -x
 
 echo "Welcome to Flip Coin Simulation Problem"
+
+a=$(( RANDOM%2 ));
+if [ $a == 1 ];
+then
+         echo "Head is winner"
+else
+        echo "Tail is winner"
+fi
